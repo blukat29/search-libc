e6ad84e616f10068f4537086fcd0029da9eec52a
blukat29 <yunjong@kaist.ac.kr>
2017-02-03 20:33:18 +0900

    Add plus and minus buttons

--- app/search/static/main.js
+++ app/search/static/main.js
@@ -1,1 +1,30 @@
-console.log('hihi');
+"use strict"
+
+function add_query(name, addr) {
+    name = name || "";
+    addr = addr || "";
+
+    var form = $('<form class="form-inline"></form>');
+    var e_name = $('<input type="text" class="form-control query-name" value="'
+            + name + '"/>');
+    var e_addr = $('<input type="text" class="form-control query-addr" value="'
+            + addr + '"/>');
+    var del_btn = $('<button type="button" class="query-del btn btn-danger">-</button>');
+    form.append(e_name);
+    form.append(e_addr);
+    form.append(del_btn);
+    $('#queries').append(form);
+}
+
+$('body').on('click', '.query-del', function(e) {
+    $(this).parent('form').remove();
+});
+
+$('#query-add').click(function() {
+    add_query();
+});
+
+$(document).ready(function() {
+    add_query("__libc_start_main_ret", "f45");
+    add_query("printf", "340");
+});
