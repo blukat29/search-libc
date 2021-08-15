94ecb0bf41409b7e4480f91589172f6c219d8633
Yunjong Jeong <yunjong@kaist.ac.kr>
2021-08-15 11:45:59 +0900

    Allow more options to docker build command

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-TAG=$(git rev-parse --short HEAD || echo 'latest')
-
 GET_DB=${GET_DB:-0}
 
-DOCKER_BUILDKIT=1 docker build --build-arg GET_DB=$GET_DB -t libc:$TAG .
+TAG=$(git rev-parse --short HEAD || echo 'latest')
+if [ "$GET_DB" != "1" ]; then
+    TAG="${TAG}-nodb"
+fi
+
+# To use fixed IP for mirror.centos.org, run
+# EXTRA_OPTS="--add-host=mirror.centos.org:113.29.189.165" GET_DB=1 ./build.sh
+
+set -ex
+
+DOCKER_BUILDKIT=1 docker build $EXTRA_OPTS --build-arg GET_DB=$GET_DB -t libc:$TAG .
 docker tag libc:$TAG libc:latest
